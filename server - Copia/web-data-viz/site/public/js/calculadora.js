de93336fdf36400e0885960ed45a88ab2677b390
anabueno25 <125586664+anabueno25@users.noreply.github.com>
2023-05-31 09:34:29 -0300

    Alterações de novo

---
+++ server - Copia/web-data-viz/site/public/js/calculadora.js
@@ -0,0 +1,188 @@
+var salvarResultado = 0;
+var atividade = 0;
+
+
+function calcular() {
+    var select = document.querySelector('#genero');
+    var optionValue = select.options[select.selectedIndex];
+    var nomeGenero = (select.value);
+
+    /*var nutricaoPessoa = {
+        id: sessionStorage(nomeUsuario),
+        genero: nomeGenero,
+        idade: idade,
+        altura: altura,
+        peso: peso,
+        atividade: nomeAtividade
+    }*/
+
+    var idade = Number(ipt_idade.value);
+    var altura = Number(ipt_altura.value);
+    var peso = Number(ipt_peso.value);
+
+    var select1 = document.querySelector('#atividade');
+    var optionValue1 = select1.options[select1.selectedIndex];
+    var nomeAtividade = select1.value;
+    var total1 = (59.512 * peso) - 30.4;
+    var total2 = (58.317 * peso) - 31.1;
+    var total3 = Number((22.706 * peso) + Number(504.3));
+    var total4 = Number((20.315 * peso) + Number(485.9));
+    var total5 = Number((17.686 * peso) + Number(658.2));
+    var total6 = Number((13.384 * peso) + Number(692.6));
+    var total7 = Number((15.057 * peso) + Number(692.2));
+    var total8 = Number((14.818 * peso) + Number(486.6));
+    var total9 = Number((11.472 * peso) + Number(873.1));
+    var total10 = Number((8.126 * peso) + Number(845.6));
+    var total11 = Number((11.711 * peso) + Number(587.7));
+    var total12 = Number((9.082 * peso) + Number(658.5));
+    var imc = (peso / (altura * altura) * 10000).toFixed(2);
+
+
+    if (nomeGenero == 'Masculino' && idade >= 0 && idade < 3) {
+        salvarResultado = total1;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total1} calorias em repouso. `;
+    }
+    if (nomeGenero == 'Feminino' && idade >= 0 && idade < 3) {
+        salvarResultado = total2;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total2} calorias em repouso`;
+    }
+    if (nomeGenero == 'Masculino' && idade >= 3 && idade < 10) {
+        salvarResultado = total3;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total3} calorias em repouso`;
+    }
+    if (nomeGenero == 'Feminino' && idade >= 3 && idade < 10) {
+        salvarResultado = total4;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total4} calorias em repouso`;
+    }
+    if (nomeGenero == 'Masculino' && idade >= 10 && idade < 18) {
+        salvarResultado = total5;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total5} calorias em repouso`;
+    }
+    if (nomeGenero == 'Feminino' && idade >= 10 && idade < 18) {
+        salvarResultado = total6;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total6} calorias em repouso`;
+    }
+    if (nomeGenero == 'Masculino' && idade >= 18 && idade < 30) {
+        salvarResultado = total7;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total7} calorias em repouso`;
+    }
+    if (nomeGenero == 'Feminino' && idade >= 18 && idade < 30) {
+        salvarResultado = total8;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total8} calorias em repouso`;
+    }
+    if (nomeGenero == 'Masculino' && idade >= 30 && idade < 60) {
+        salvarResultado = total9;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total9} calorias em repouso`;
+    }
+    if (nomeGenero == 'Feminino' && idade >= 30 && idade < 60) {
+        salvarResultado = total10;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total10} calorias em repouso`;
+    }
+    if (nomeGenero == 'Masculino' && idade >= 60) {
+        salvarResultado = total11;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total11} calorias em repouso`;
+    }
+    if (nomeGenero == 'Feminino' && idade >= 60) {
+        salvarResultado = total12;
+        document.getElementById('ipt_taxaMb').value = `Você gasta ${total12} calorias em repouso`;
+    }
+
+    if (nomeAtividade == 'sedentaria') {
+        atividade = salvarResultado * 1.2;
+        document.getElementById('ipt_tdee').value = `Você gasta ${atividade} em atividade física`;
+    }
+    else if (nomeAtividade == 'atvLeve') {
+        atividade = salvarResultado * 1.375;
+        document.getElementById('ipt_tdee').value = `Você gasta ${atividade} em atividade física`;
+    }
+    else if (nomeAtividade == 'atvModerada') {
+        atividade = salvarResultado * 1.55;
+        document.getElementById('ipt_tdee').value = `Você gasta ${atividade} em atividade física`;
+    }
+    else if (nomeAtividade == 'atvAlta') {
+        atividade = salvarResultado * 1.725;
+        document.getElementById('ipt_tdee').value = `Você gasta ${atividade} em atividade física`;
+    }
+    else if (nomeAtividade == 'atvExtrema') {
+        atividade = salvarResultado * 1.9;
+        document.getElementById('ipt_tdee').value = `Você gasta ${atividade} em atividade física`;
+    }
+
+    if (imc < 18.5) {
+        document.getElementById('ipt_imc').value = `Seu imc é ${imc}`;
+        document.getElementById('ipt_classificacao').value = `Magreza`;
+    }
+    else if (imc >= 18.5 && imc <= 24.9) {
+        document.getElementById('ipt_imc').value = `Seu imc é ${imc}`;
+        document.getElementById('ipt_classificacao').value = `Peso normal`;
+    }
+    else if (imc >= 25 && imc <= 29.9) {
+        document.getElementById('ipt_imc').value = `Seu imc é ${imc}`;
+        document.getElementById('ipt_classificacao').value = `SobrePeso`;
+    }
+    else if (imc >= 30 && imc <= 34.9) {
+        document.getElementById('ipt_imc').value = `Seu imc é ${imc}`;
+        document.getElementById('ipt_classificacao').value = `Obesidade grau I`;
+    }
+    else if (imc >= 35 && imc <= 40) {
+        document.getElementById('ipt_imc').value = `Seu imc é ${imc}`;
+        document.getElementById('ipt_classificacao').value = `Obesidade grau II`;
+    }
+    else if (imc >= 40) {
+        document.getElementById('ipt_imc').value = `Seu imc é ${imc}`;
+        document.getElementById('ipt_classificacao').value = `Obesidade grau III`;
+    }
+
+
+
+
+    //Recupere o valor da nova input pelo nome do id
+    // Agora vá para o método fetch logo abaixo
+    var idUsuarioVar = sessionStorage.ID_USUARIO;
+    var imcVar = imc;
+    var salvarResultadoVar = salvarResultado;
+    var atividadeVar = atividade;
+
+    if (idUsuarioVar == "" || imcVar == "" || salvarResultadoVar == "" || atividadeVar == "") {
+        mensagem_erro.innerHTML = "Todos os seus campos estão em branco! Favor inserir os dados corretamente.";
+
+        return false;
+    }
+
+    // Enviando o valor da nova input
+    fetch("/usuarios/calcular", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            // crie um atributo que recebe o valor recuperado aqui
+            // Agora vá para o arquivo routes/usuario.js
+            salvarResultadoServer: salvarResultadoVar,
+            atividadeServer: atividadeVar,
+            imcServer: imcVar,
+            idUsuarioServer: idUsuarioVar
+        })
+    }).then(function (resposta) {
+
+        console.log("resposta: ", resposta);
+
+        if (resposta.ok) {
+            alert('inseriu');
+            mensagem_erro.innerHTML = "Cadastro realizado com sucesso! Redirecionando para tela de Login...";
+
+
+        } else {
+            throw ("Houve um erro ao tentar realizar o cadastro!");
+        }
+    }).catch(function (resposta) {
+        console.log(`#ERRO: ${resposta}`);
+    });
+
+    return false;
+}
+
+
+
+
+
---
+++ server - Copia/web-data-viz/site/public/js/quiz.js
@@ -0,0 +1,242 @@
+let titulo = document.querySelector('h1')
+let instrucoes = document.querySelector('#instrucoes')
+let aviso = document.querySelector('#aviso')
+//let respostaEsta = document.querySelector('#respostaEsta')
+let pontos = 0 // pontos para o placar
+let placar = 0 // placar
+
+// PERGUNTA
+let numQuestao = document.querySelector('#numQuestao')
+let pergunta   = document.querySelector('#pergunta')
+
+// ALTERNATIVAS
+let a = document.querySelector('#a')
+let b = document.querySelector('#b')
+
+
+// article com a class questoes
+let articleQuestoes = document.querySelector('.questoes')
+// ol li com as alternativas
+let alternativas = document.querySelector('#alternativas')
+
+const q0 = {
+    numQuestao   : 0,
+    pergunta     : "Pergunta",
+    alternativaA : "Alternativa A",
+    alternativaB : "Alternativa B",
+    correta      : "0",
+}
+
+const q1 = {
+    numQuestao   : 1,
+    pergunta     : "Você gosta de couve-flor?",
+    alternativaA : "Sim, eu gosto!",
+    alternativaB : "Não, credo!",
+    correta      : "Sim, eu gosto!",
+}
+
+const q2 = {
+    numQuestao   : 2,
+    pergunta     : "Você gosta de ervilha? ",
+    alternativaA : "Sim, eu gosto!",
+    alternativaB : "Não, saí pra lá!",
+    correta      : "Sim, eu gosto!",
+}
+
+const q3 = {
+    numQuestao   : 3,
+    pergunta     : "Você gosta de abacate?",
+    alternativaA : "Sim, eu gosto",
+    alternativaB : "Não, é muito amargo",
+    correta      : "Sim, eu gosto",
+}
+
+const q4 = {
+    numQuestao   : 4,
+    pergunta     : "Você gosta de beterraba?",
+    alternativaA : "Sim, eu gosto",
+    alternativaB : "Não, não tem gosto de nada!",
+    correta      : "Sim, eu gosto",
+}
+
+const q5 = {
+    numQuestao   : 5,
+    pergunta     : "Você gosta de quiabo?",
+    alternativaA : "Sim, eu gosto",
+    alternativaB : "Não, acho péssimo!",
+    correta      : "Sim, eu gosto",
+}
+
+const q6 = {
+    numQuestao   : 6,
+    pergunta     : "Você gosta de beringela?",
+    alternativaA : "Sim, eu gosto",
+    alternativaB : "Não, tem uma testura esquisita!",
+    correta      : "Sim, eu gosto",
+}
+
+const q7 = {
+    numQuestao   : 7,
+    pergunta     : "Você gosta de rúcula?",
+    alternativaA : "Sim, eu gosto",
+    alternativaB : "Não, é ruim!",
+    correta      : "Sim, eu gosto",
+}
+
+const q8 = {
+    numQuestao   : 8,
+    pergunta     : "Você gosta de chuchu?",
+    alternativaA : "Sim, eu gosto",
+    alternativaB : "Não, credo!",
+    correta      : "Sim, eu gosto",
+}
+
+const q9 = {
+    numQuestao   : 9,
+    pergunta     : "Você gosta de pepino?",
+    alternativaA : "Sim, eu gosto",
+    alternativaB : "Não, tem um gosto estranho!",
+    correta      : "Sim, eu gosto",
+}
+
+const q10 = {
+    numQuestao   : 10,
+    pergunta     : "Você gosta de café sem açúcar?",
+    alternativaA : "Sim, até prefiro!",
+    alternativaB : "Eu não, credo!",
+    correta      : "Sim, até prefiro!",
+}
+
+
+
+
+// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
+const questoes = [q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
+
+let numero = document.querySelector('#numero')
+let total  = document.querySelector('#total')
+
+numero.textContent = q1.numQuestao
+
+let totalDeQuestoes = (questoes.length)-1
+console.log("Total de questões " + totalDeQuestoes)
+total.textContent = totalDeQuestoes
+
+// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
+numQuestao.textContent = q1.numQuestao
+pergunta.textContent   = q1.pergunta
+a.textContent = q1.alternativaA
+b.textContent = q1.alternativaB
+
+
+// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
+a.setAttribute('value', '1A')
+b.setAttribute('value', '1B')
+
+
+// PARA MONTAR AS PROXIMAS QUESTOES
+function proximaQuestao(nQuestao) {
+    numero.textContent = nQuestao
+    numQuestao.textContent = questoes[nQuestao].numQuestao
+    pergunta.textContent   = questoes[nQuestao].pergunta
+    a.textContent = questoes[nQuestao].alternativaA
+    b.textContent = questoes[nQuestao].alternativaB
+    a.setAttribute('value', nQuestao+'A')
+    b.setAttribute('value', nQuestao+'B')
+}
+
+function bloquearAlternativas() {
+    a.classList.add('bloqueado')
+    b.classList.add('bloqueado')
+}
+
+function desbloquearAlternativas() {
+    a.classList.remove('bloqueado')
+    b.classList.remove('bloqueado')
+}
+
+function verificarSeAcertou(nQuestao, resposta) {
+
+    let numeroDaQuestao = nQuestao.value
+    console.log("Questão " + numeroDaQuestao)
+
+    let respostaEscolhida = resposta.textContent
+    //console.log("RespU " + respostaEscolhida)
+
+    let certa = questoes[numeroDaQuestao].correta
+    //console.log("RespC " + certa)
+
+    if(respostaEscolhida == certa) {
+        //console.log("Acertou")
+        //respostaEsta.textContent = "Correta 😊"
+        pontos += 10 // pontos = pontos + 10
+    } else {
+        //console.log("Errou!")
+        //respostaEsta.textContent = "Errada 😢"
+    }
+
+    // atualizar placar
+    placar = pontos
+    instrucoes.textContent = "Pontos " + placar
+
+    // bloquear a escolha de opcoes
+    bloquearAlternativas()
+
+    setTimeout(function() {
+        //respostaEsta.textContent = '...'
+        proxima = numeroDaQuestao+1
+
+        if(proxima > totalDeQuestoes) {
+            console.log('Fim do Jogo!')
+            fimDoJogo()
+        } else {
+            proximaQuestao(proxima)
+        }
+    }, 250)
+    desbloquearAlternativas()
+}
+
+function fimDoJogo() {
+    instrucoes.textContent = "Fim de Jogo!"
+    numQuestao.textContent = ""
+
+    let pont = ''
+    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
+
+    pergunta.textContent   = "Você conseguiu <br>" + pontos + " " + pont
+
+    aviso.textContent = `Você conseguiu ${pontos} pontos! `;
+
+    if (pontos <= 30) {
+        aviso.textContent += `Você precisa reavaliar a sua alimentação!!`;
+    }
+    if (pontos >= 40 && pontos <=60) {
+        aviso.textContent += `Você não está tão ruim, mas pode melhorar sua alimentação!!`;
+    }
+    if (pontos >= 70 && pontos <=80) {
+        aviso.textContent += `Olha, você está quase lá, sua alimentação é boa! Mas que tal experimentar os alimentos que não gosta novamente?`;
+    }
+    if (pontos >= 90) {
+        aviso.textContent += `Excelente! Parabéns, você come de tudo!`;
+    }
+
+    a.textContent = ""
+    b.textContent = ""
+
+
+    a.setAttribute('value', '0')
+    b.setAttribute('value', '0')
+
+
+    // OCULTAR O ARTICLE DA QUESTAO
+    articleQuestoes.style.display = 'none'
+
+    gravarPontos();
+
+    setTimeout(function() {
+        pontos = 0 // zerar placar
+        location.reload();
+    }, 10000)
+    
+}
+
